8acc51123712d08ee4f471628beb87218becda23
Mark S <marksherm04@gmail.com>
2021-07-06 14:23:55 -0400

    deleted routes, trying to run all on server.js to see if it works

--- server.js
+++ server.js
@@ -19,6 +19,49 @@ app.use(express.json());
 // using middleware package
 app.use(express.static("./public"));
 
+// / callback function executing everytime homepage index.html route is accessed with GET request
+app.get("*", (req, res) => {
+	res.sendFile(path.join(_dirname, "./public/index.html")) // may need to add ./public in front -- going to test
+});
+
+// callback function executing everytime notes route is accessed with GET request
+app.get("/notes", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/notes.html")) // may need to add ./public in front -- going to test
+});
+
+
+// API routes -- GET routes
+app.get("/api/notes", (req, res) => {
+	fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+		if (err) {
+			throw (err)
+		}
+		else {
+			const userNote = JSON.parse(data);
+			res.send(userNote);
+		}
+	});
+});
+
+app.post("/api/notes", (req,res) => {
+	const userNote = req.body;
+	notes.push(userNote);
+	fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+		const noteContent = JSON.parse(data);
+		const newNote = [...noteContent, userNote];
+		JSON.stringify(newNote);
+		res.send(newNote);
+		fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newNote), (err, data) => {
+			if (err) {
+				throw (err);
+			}
+		});
+	});
+});
+
+
+
+
 app.listen(PORT, () => {
 	console.log(`API server now on port ${PORT}!`);
 });
