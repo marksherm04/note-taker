44797f71e150faf4c0dc1c14131e7c1f80f49fa0
Mark S <marksherm04@gmail.com>
2021-07-06 13:13:18 -0400

    deleted routes folderas I don't think they are needed here, started server.js app use and get code, added const packages

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
-
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3001;
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
+
+// using middleware package
+app.use(express.static("./public"));
+
+// go to homepage index.html
+app.get("/", (req, res) => {
+	res.sendFile(path.join(_dirname, "./public/index.html"))
+});
+
 
 
 
